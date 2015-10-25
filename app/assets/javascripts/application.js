f782d7605cffdf56074ace008734bfe7b4326a43
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-10-25 14:45:35 -0400

    Create initial half donut chart.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,47 +21,38 @@ function generateHalfDonuts() {
       height = 150,
       radius = Math.min(width, height) / 2;
 
-  var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
   var arc = d3.svg.arc()
       .outerRadius(radius - 10)
-      .innerRadius(radius - 70);
+      .innerRadius(radius - 40);
 
   var pie = d3.layout.pie()
       .sort(null)
-      .value(function(d) { return d.population; });
+      .startAngle(-1.25*Math.PI/2)
+      .endAngle(1.25*Math.PI/2)
+      .value(function(d) { return d.percentage; });
 
   var svg = d3.selectAll("svg.half-donut")
       .attr("width", width)
       .attr("height", height)
     .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-  //
-  // var jsonData = {yes: 40, no: 60}
-  //
-  // d3.json(jsonData, function(error, data) {
-  //
-  //   data.forEach(function(d) {
-  //     d.population = +d.population;
-  //   });
-  //
-  //   var g = svg.selectAll(".arc")
-  //       .data(pie(data))
-  //     .enter().append("g")
-  //       .attr("class", "arc");
-  //
-  //   g.append("path")
-  //       .attr("d", arc)
-  //       .style("fill", function(d) { return color(d.data.age); });
-  //
-  //   g.append("text")
-  //       .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-  //       .attr("dy", ".35em")
-  //       .style("text-anchor", "middle")
-  //       .text(function(d) { return d.data.age; });
-  //
-  // });
+
+  var jsonData = [{name: "Yes", percentage: 40}, {name: "No", percentage: 60}];
+
+  var g = svg.selectAll(".arc")
+      .data(pie(jsonData))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return d.data.name=="Yes" ? "green" : "red"; });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.data.name; });
 }
 
 $(generateHalfDonuts);
