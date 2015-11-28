9550c9d59fb60e4b13d3f9941e350870f4e7a568
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-11-27 21:56:14 -0500

    Set up initial calculation for working thumbs into claims.

--- app/models/proposition.rb
+++ app/models/proposition.rb
@@ -13,9 +13,16 @@ class Proposition < ActiveRecord::Base
 
   default_scope -> {order(created_at: :desc)}
 
+  def thumb_count
+    bools = {true: 1, false: -1}
+    claims.joins(:thumbs)
+        .select("claims.positive AS positive, thumbs.up AS up")
+        .reduce(0) {|sum, c| sum + (c.positive ? 1 : -1)*(c.up ? 1 : -1) }
+  end
+
   def percentage_agreement
     if claims.count > 0
-      100.0 * claims.where(positive: true).count / claims.count
+      thumb_count + 100.0 * claims.where(positive: true).count / claims.count
     else
       0
     end
