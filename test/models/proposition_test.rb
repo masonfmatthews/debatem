ff54c9f3bb1e0ee91128623664f9415acc2f4410
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-12-01 14:28:17 -0500

    Set up tests.

--- test/models/proposition_test.rb
+++ test/models/proposition_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class PropositionTest < ActiveSupport::TestCase
-  # test "percentage agreement" do
-  #   assert_equal 50, propositions(:one).percentage_agreement
-  #   positions(:two).destroy
-  #   assert_equal 0, propositions(:one).percentage_agreement
-  # end
+  test "percentage agreement" do
+    assert_equal 50, propositions(:one).percentage_agreement
+    claims(:two).destroy
+    assert_equal 0, propositions(:one).reload.percentage_agreement
+  end
 end
